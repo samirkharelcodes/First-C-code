3910263dfea6c87c6e6bc4417ec27261208c1469
Samir Kharel <samirkharel975@gmail.com>
2026-02-24 17:36:08 +0545

    Add files via upload

---
+++ sum-of-2-numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    int i,j,sum;
+
+    printf("Enter any two numbers.\n");
+    scanf("%d%d",&i,&j);
+    sum=i+j;
+    printf("The sum of your numbers is %d.",sum);
+
+return 0;
+
+}
